98cc487c919e4e7d118c1ababf8bb0ee1aa70382
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2021-07-10 13:10:03 +0530

    Create km_to_m.java

---
+++ km_to_m.java
@@ -0,0 +1,13 @@
+package com.sank;
+import java.util.Scanner;
+public class Main {
+//perfect number
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the distance in  KM");
+	int km=sc.nextInt();
+	int m = km*1000;
+	System.out.println("meter "+ m);
+
+    }
+}
