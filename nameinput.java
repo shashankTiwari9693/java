6b568f4dc21dd3154eb9225208dac4be610b33b0
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2021-07-10 12:47:24 +0530

    Create nameinput.java

---
+++ nameinput.java
@@ -0,0 +1,11 @@
+package com.sank;
+import java.util.Scanner;
+public class Main {
+//perfect number
+    public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter your name");
+	String name=sc.nextLine();
+	System.out.println("Hello " + name +",have a good day" );
+    }
+}
