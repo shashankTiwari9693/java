c15d76a004ee25b9478308f6e83db489602ea3e9
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2021-07-23 21:39:29 +0530

    Create days.java

---
+++ days.java
@@ -0,0 +1,46 @@
+package com.sank;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+
+     System.out.println("enter the numb of day");
+     int day = sc.nextInt();
+     switch(day) {
+         case 1:
+             System.out.println("sunday");
+             break;
+
+         case 2:
+             System.out.println("Monday");
+             break;
+
+         case 3:
+             System.out.println("Tuesday");
+             break;
+
+         case 4:
+             System.out.println("Wednesday");
+             break;
+
+         case 5:
+             System.out.println("Thursday");
+             break;
+         case 6:
+             System.out.println("Friday");
+             break;
+         case 7:
+             System.out.println("Saturday");
+             break;
+         default:
+             System.out.println("no day");
+
+
+     }
+
+    }
+
+    }
+
