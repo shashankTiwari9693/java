aa020cfe1e36c1ed78e52bf4d48d89803eec595a
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2021-07-29 00:25:01 +0530

    Create Array_reverse.java

---
+++ Array_reverse.java
@@ -0,0 +1,16 @@
+package com.sank;
+public class Main {
+
+    public static void main(String[] args) {
+    int []marks ={12,43,42,53,63};
+    for(int i=marks.length-1;i>=0;i--)
+    {
+        System.out.println(marks[i]);
+    }
+
+
+
+    }
+
+    }
+
