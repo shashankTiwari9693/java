5c5605c7ec3c983f5d213af41b9ff4db5a5f3546
Shashank Tiwari <67553585+sanktiwarirobotics@users.noreply.github.com>
2021-07-29 22:46:19 +0530

    Create  print_the_following_grid.java

---
+++ print_the_following_grid.java
@@ -0,0 +1,37 @@
+
+package com.sank;
+import java.util.Arrays;
+
+public class Main {
+//Write a Java program to sort a numeric array and a string array
+
+    public static void main(String[] args) {
+    int [][] das= new int[10][10];
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++)
+        {
+            System.out.print("-");
+            System.out.print(" ");
+        }
+        System.out.println(" ");
+    }
+    }
+
+    }
+
+/*
+"C:\Program Files\Java\jdk-16.0.1\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2021.1.3\lib\idea_rt.jar=52691:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2021.1.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\19282\Desktop\code\Practice\out\production\Practice com.sank.Main
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+- - - - - - - - - -  
+
+
+*/
+
